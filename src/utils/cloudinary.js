93066e901da95842b2b998e7fce77fcb10d18723
Narasimha <Narasimhapalla729@gmail.com>
2023-12-25 07:50:44 +0530

    cloudinary && multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+//File handling
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const res = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfullly
+    console.log("File is upoaded on cloudinary", res.url);
+    return res;
+  } catch (err) {
+    fs.unlinkSync(localFilePath); // Remove the locally saved temp file as the upload operation got failed
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
